1738739a0a7f90e2e34fa6cf1881bbc906fd446e
johnnyghost <kemarx@gmail.com>
2016-02-24 14:17:28 +0000

    chore: update.

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    publicPath: '/dist',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    inline: true,
+    port: 3000
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
+  }
+}
