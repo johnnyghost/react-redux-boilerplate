7ac07ce44dbfbdbaff58a1a2e706680c5062611e
johnnyghost <kemarx@gmail.com>
2016-03-09 22:23:53 +0000

    feat(auth): wip

---
+++ src/views/auth/Auth.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AuthView extends Component {
+  render() {
+    return (
+      <section className="sleep row">
+        <h2>Auth</h2>
+      </section>
+    );
+  }
+}
+
+export default AuthView;
